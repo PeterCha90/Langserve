fc4e7f01b8b231d0fb2c0adac195572b52816665
Peter Cha <petercha90@gmail.com>
2026-02-14 15:40:47 +0900

    chore: xionic 코드 제거

--- app/server.py
+++ app/server.py
@@ -5,7 +5,6 @@ from typing import List, Union
 from langserve import add_routes
 from rag import chain as rag_chain
 from chat import chain as chat_chain
-from xionic import chain as xionic_chain
 from translator import chain as EN_TO_KO_chain
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -57,14 +56,6 @@ add_routes(app, EN_TO_KO_chain, path="/translate")
 add_routes(app, rag_chain, path="/rag", enable_feedback_endpoint=True,
            enable_public_trace_link_endpoint=True,)
 add_routes(app, model, path="/llm")
-add_routes(
-    app,
-    xionic_chain.with_types(input_type=InputChat),
-    path="/xionic",
-    enable_feedback_endpoint=True,
-    enable_public_trace_link_endpoint=True,
-    playground_type="chat",
-)
 
 if __name__ == "__main__":
     import uvicorn
